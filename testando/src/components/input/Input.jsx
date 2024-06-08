0b752dd644ae5c24434aa22a066bc84fca9bbbad
Nicolas645 <nicolas.barboza@outlook.com>
2024-06-08 00:07:16 -0300

    Estilização da página de criar conta

--- testando/src/components/input/Input.jsx
+++ testando/src/components/input/Input.jsx
@@ -1,9 +1,9 @@
 import styles from "./input.module.css"
 
-export function InputModificado({name, onChange, value}){
+export function InputModificado1({name, onChange, value}){
     return(
         <>
-        <input name={name} placeholder="Teste" onChange={onChange} value={value} className={styles.InputTeste}>
+        <input name={name} placeholder="Nome" onChange={onChange} value={value} className={styles.Input2}>
         
         </input>
         </>
@@ -13,7 +13,43 @@ export function InputModificado({name, onChange, value}){
 export function InputModificado2({name, onChange, value}){
     return(
         <>
-        <input name={name} placeholder="Teste" onChange={onChange} value={value} className={styles.Input2}>
+        <input name={name} placeholder="Telefone" onChange={onChange} value={value} className={styles.Input2}>
+        
+        </input>
+        </>
+    )
+}
+export function InputModificado3({name, onChange, value}){
+    return(
+        <>
+        <input name={name} placeholder="E-mail" onChange={onChange} value={value} className={styles.Input2}>
+        
+        </input>
+        </>
+    )
+}
+export function InputModificado4({name, onChange, value}){
+    return(
+        <>
+        <input name={name} placeholder="CPF" onChange={onChange} value={value} className={styles.Input2}>
+        
+        </input>
+        </>
+    )
+}
+export function InputModificado5({name, onChange, value}){
+    return(
+        <>
+        <input name={name} placeholder="Senha" onChange={onChange} value={value} className={styles.Input2}>
+        
+        </input>
+        </>
+    )
+}
+export function InputModificado6({name, onChange, value}){
+    return(
+        <>
+        <input name={name} placeholder="CEP" onChange={onChange} value={value} className={styles.Input2}>
         
         </input>
         </>
