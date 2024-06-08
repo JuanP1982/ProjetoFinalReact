88cb12b552a2f2fae97c19e5e9b8c3283cc03b1b
Juan Pedro <juanpedrovsc@gmail.com>
2024-06-08 05:20:06 -0300

    Atualizando logica

--- testando/src/pages/Perfil/Perfil.jsx
+++ testando/src/pages/Perfil/Perfil.jsx
@@ -1,34 +1,53 @@
 import { useContext, useEffect, useState } from "react"
-import { getClientes } from "../../service/clientes"
+import { getClienteId, getClientes } from "../../service/clientes"
 import { Card } from "../../components/Card/Card"
 import { CardBox } from "../../components/Card/style"
 import { ClienteContext } from "../../context/clienteContext"
-import { limparLocalStorage, obterPefilUsuario } from "../../uteis/localStorage/localStorage"
+import { limparLocalStorage, obterPefilUsuario, obterToken } from "../../uteis/localStorage/localStorage"
+
 
 export function ListarClientes(){
-    const [cliente, setCliente] = useState()
+    const [cliente, setCliente] = useState(undefined)
 
     useEffect(()=>{
+        if (localStorage.getItem("token") === null) {
+            document.body.style.display = "hidden"
+            alert("Faça login para continuar!")
+            setInterval(()=>{window.location.href="/cadastro"},1000)
+        }
+
         const clienteCarregar = obterPefilUsuario()
         setCliente(clienteCarregar)
-        console.log(cliente)
-    }, [])
+        
+        const clienteAtualizar = async () =>{
+            const clienteAtualizado = await getClienteId()
+            setCliente(clienteAtualizado)
+        }
 
+        if (clienteCarregar) {
+            clienteAtualizar()
+        }
+
+    }, [])
+    
     if (cliente === undefined) {
         return <div>...Carregando...</div>
     }
     
     return(
         <div>
-           <CardBox>
-            <p key={cliente.id}>ID: {cliente.id}</p>
+           <CardBox key={cliente.id}>
+            <p >ID: {cliente.id}</p>
            <p >Nome: {cliente.nome}</p>
            <p >Email: {cliente.email}</p>
             <p >Telefone: {cliente.telefone}</p>
-            <p>Logradouro:<i> {cliente.logradouro}</i></p>
-            <p>Pedidos: {cliente.pedido.map((pedido)=>{
+            <p>Logradouro: {cliente.logradouro}</p>
+             {cliente.pedido.map((pedido, index)=>(
+                <div key={index}>
                 <p>{pedido.totalCarrinho}</p>
-            })}</p>
+                <p>{pedido.status}</p>
+                </div>
+            ))}
             </CardBox>
          <button type="button" onClick={()=>{limparLocalStorage()}}>Log-out</button>
          <br/>
--- testando/src/service/clientes.js
+++ testando/src/service/clientes.js
@@ -1,25 +1,40 @@
-import { salvarPerfilUsuario, salvarToken } from "../uteis/localStorage/localStorage"
+import { obterPefilUsuario, salvarPerfilUsuario, salvarToken } from "../uteis/localStorage/localStorage"
 import { api } from "./api"
 
    
-    export const postCliente= (cliente) => {
+    export const postCliente= async (cliente) => {
         const url = '/clientes'
-        return api.post(url,cliente,{
-            headers:{"Access-Control-Allow-Origin*":""}
-        }).then((res)=>{
-            console.log("Cliente Criado com sucesso", res);
-            salvarPerfilUsuario(res.data);
+        try {
+            const res = await api.post(url, cliente, {
+                headers: { "Access-Control-Allow-Origin*": "" }
+            })
+            console.log("Cliente Criado com sucesso", res)
+            salvarPerfilUsuario(res.data)
             salvarToken("Logado")
-            setTimeout(()=> (window.location.href="/perfil"),2000)
+            setTimeout(() => (window.location.href = "/perfil"), 2000)
             return alert(`Bem vindo, ${cliente.nome}!`)
-        }).catch((err) =>{
+        } catch (err) {
             console.error("Erro ao criar cliente", err)
-        })
+        }
     }
 
-export const getClientes= (cliente)=> {
+export const getClientes= ()=> {
     const url = "/clientes"
-    return api.get(url, {
-        headers:{"Access-Control-Allow-Origin*":""}
-    })
+    return api.get(url)
 }
+
+export const getClienteId= async ()=> {
+    const cliente = obterPefilUsuario()
+    if(!cliente || cliente === null){
+        console.error("Erro ao pegar cliente")
+    }
+    const url = `/clientes/${cliente.id}`
+    try {
+        const res = await api.get(url)
+        salvarPerfilUsuario(res.data)
+        return res.data
+    } catch (err) {
+        console.error("Erro ao obeter informações")
+    }
+}
+
