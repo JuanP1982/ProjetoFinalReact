4956e90e681e803bfd35e8a20fb7a96634b5e4ac
leony <leo.ny2010@hotmail.com>
2024-06-10 21:12:12 -0300

    atualizacao

--- testando/src/components/Card/style.js
+++ testando/src/components/Card/style.js
@@ -3,16 +3,16 @@ import styled from "styled-components";
 export const CardBox = styled.div`
 border: 1px solid #ccc;
     border-radius: 25px;
-    padding: 1%;
-    width: 20%;
+    padding: 2%;
+    flex: 1 1 calc(20% - 4rem);
     background-color: black;
-    margin: 2.6rem;
+    margin: 2rem;
     color: white;
-    margin-top: 10%;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    transition: background-color 0.3s ease,color 0.3 ease;
     transition: transform 0.3s ease, box-shadow 0.3s ease, background-color 0.3s ease, color 0.3s ease;
+    margin-top: 10% ;
     
+
 
     &:hover {
     background-color:rgba(46, 41, 41, 0.438);
@@ -21,14 +21,14 @@ border: 1px solid #ccc;
     box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
   }
   @media (max-width: 1200px) {
-        width: 45%; 
+        flex: 1 1 calc(33.33% - 2rem); 
+    }
 
     @media (max-width: 768px) {
-        width: 100%; 
+        flex: 1 1 calc(50% - 2rem); 
     }
-  }
+
+    @media (max-width: 480px) {
+        flex: 1 1 calc(100% - 2rem);
+    }
 `;
-   
-
-
-
