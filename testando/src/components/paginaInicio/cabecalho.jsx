5a09ffe8d616b4dbe834b01fd76d285069bcc161
Juan Pedro <juanpedrovsc@gmail.com>
2024-06-11 20:08:25 -0300

    Atualizando nav bar

--- testando/src/components/paginaInicio/cabecalho.jsx
+++ testando/src/components/paginaInicio/cabecalho.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {obterPefilUsuario} from "../../uteis/localStorage/localStorage"
 import "./cabecalho.css";
 
 const Cabecalho = ({ busca, setBusca }) => {
+    const [cliente, setCliente] = useState(obterPefilUsuario())
+    const [mensagem, setMensagem] = useState("")
+    const [mensagem2, setMensagem2] = useState("")
+    const [link,setLink] = useState("")
+    const [link2,setLink2] = useState("")
+    useEffect(()=>{
+
+    if (localStorage.getItem("token")) {
+        const nomeCliente = cliente.nome;
+        const nomeLimitado = `${nomeCliente.substring(0, 10)}...`
+        setMensagem(`Bem vindo(a), ${nomeLimitado}`)
+        setLink("/")
+        setMensagem2("perfil")
+        setLink2("/perfil")
+        return;
+    }else{
+        setMensagem("Registre-se")
+        setLink("/cadastro")
+        setMensagem2("login")
+        setLink2("/login")
+    }
+   },[])
+
+   if (cliente === undefined) {
+    return <div>...Carregando...</div>
+}
+    
     return (
         <header>
             <nav>
@@ -20,8 +48,8 @@ const Cabecalho = ({ busca, setBusca }) => {
             />
              <nav>
              <ul>
-                    <button className="buttonC" type="button" onClick={() => (window.location.href = "/cadastro")}>Registrar-se</button>
-                    <button className="buttonC" type="button" onClick={() => (window.location.href = "/perfil")}>Perfil</button>
+                    <button className="buttonC" type="button" onClick={() => (window.location.href = `${link}`)}>{mensagem}</button>
+                    <button className="buttonC" type="button" onClick={() => (window.location.href = `${link2}`)}>{mensagem2}</button>
                     </ul>
                     </nav>
         </header>
